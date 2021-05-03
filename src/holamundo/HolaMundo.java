dd3cfe67345eba7bf68acb30932395121c4c632d
Axel Enrique Martinez Duran <U631378@TECO.COM.AR>
2021-05-03 18:31:51 -0300

    inicio

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
